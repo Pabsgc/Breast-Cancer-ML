4d527f0af0fc4f9b5558bb0dfda81df179c97fbb
Pablillo <pablo.gallego.cal@gmail.com>
2026-04-17 21:37:09 +0200

    Fix of last commit

--- 2_results_load_only.py
+++ 2_results_load_only.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
